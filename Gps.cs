0c34901b978086a1f1abe262d2ca903c686f4414
Filippo Mastrotto <mastrottofilippo@gmail.com>
2019-05-02 09:50:10 +0200

    Sensore Gps

---
+++ Gps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulazione_Gps
+{
+    class Gps
+    {
+        public Gps() { }
+
+        private decimal[] PosizioneDecimale()
+        {
+            decimal[] posizione = new decimal[2];
+            Random longitudine = new Random();
+            Random latitudine = new Random();
+            
+            posizione[0] = Convert.ToDecimal(longitudine.Next(-18000000,18000000))/1000000;
+            posizione[1] = Convert.ToDecimal(latitudine.Next(-90000000, 90000000)) / 1000000;
+
+
+ 
+            return posizione;
+        }
+        public string ToJson()
+        {
+            decimal[] posizione = PosizioneDecimale();
+            string longitudine = posizione[0].ToString();
+            string latitudine = posizione[1].ToString();
+            longitudine.Replace(',', '.');
+            latitudine.Replace(',', '.');
+            return "{\"latitudine\":\"" + latitudine + "\",\"longitudine\":\"" + longitudine + "\"}";
+        }
+    }
+}
