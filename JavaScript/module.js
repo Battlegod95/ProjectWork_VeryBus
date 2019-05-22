690a224df485eeec7d8e1c1706749711a9c4614f
francescavenier <francesca.venier@tecnicosuperiorekennedy.it>
2019-05-22 15:52:05 +0200

    Aggiunto il file module.js contenente le configurazioni esterne per host e database. Modificato anche il file index.js contenuto all'interno di server con gli opportuni collegamenti al nuovo file.

---
+++ JavaScript/module.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host: function() {
+        return '192.168.101.74:8086'
+    },
+    database: function() {
+        return 'Verybus'
+    }
+}
--- JavaScript/server/index.js
+++ JavaScript/server/index.js
@@ -1,6 +1,25 @@
 // collegamento al db - è necessario "npm install --save influx"
 const sql = require('influx');
-const influx = new sql.InfluxDB('http://Verybus:password@192.168.101.74:8086/Verybus');
+const module = require('./module.js');
+let host = module.host();
+let database = module.database();
+
+//const influx = new sql.InfluxDB('http://Verybus:password@192.168.101.74:8086/Verybus');
+const influx = new sql.InfluxDB({
+    host: host,
+    database: database,
+    schema: [
+      {
+        measurement: 'DatiAutobus',
+        fields: { StatoPorta: sql.FieldType.BOOLEAN,
+                ContaPersone: sql.FieldType.INTEGER,
+                Latitudine: sql.FieldType.FLOAT,
+                Longitudine: sql.FieldType.FLOAT
+         },
+        tags: ['TargaAutobus', 'Linea']
+      }
+    ]
+  });
 
 // creazione array statico - autobus
 var situazioneAutobus = [];
@@ -14,28 +33,38 @@ async function routes(fastify, options) {
     });
 
     fastify.post('/', async (request, reply) => {
-        try {
+        
             
-            var targa = request.body.id;
-            var StatoPortaR = request.body.StatoPorta;
-            var ConteggioPersoneR = request.body.ConteggioPersone;
-            var LatitudineR = request.body.Latitudine;
-            var LongitudineR = request.body.Longitudine;
-            var linea ="";
-
+            var targaR = request.body.targa;
+            var StatoPortaR = request.body.statoporta;
+            var ConteggioPersoneR = request.body.contatorepersone;
+            var LatitudineR = request.body.latitudine;
+            var LongitudineR = request.body.longitudine;
+            var lineaR =request.body.linea;
             //situazioneAutobus.push({ id: idR, StatoPorta: StatoPortaR, ConteggioPersone: ConteggioPersoneR, Latitudine: LatitudineR, Longitudine: LongitudineR, Data: DataA, Ora: OraA });
-            influx.query(`insert DatiAutobus,TargaAutobus=@targa,Linea=@linea StatoPorta=@StatoPortaR,ContaPersone=@ContaPersoneR,Latitudine=@LatitudineR,Longitudine=@LongitudineR`)
-            .then(result => {reply.send(result)})
-            .catch(error => {reply.send(error)}); 
-        } catch (error) {
-            console.log(error);
-            reply.send();
-            reply.status(400).send("Bad request");
-            //sql.close();
-        }
+            
+            
+            
+            
+            influx.writePoints([
+                {
+                  measurement: 'DatiAutobus',
+                  tags: {
+                    TargaAutobus: targaR,
+                    Linea: lineaR,
+                  },
+                  fields: { StatoPorta: StatoPortaR,ContaPersone:ConteggioPersoneR,Latitudine:LatitudineR,Longitudine:LongitudineR }                
+                }
+              ], {
+                database: 'Verybus',               
+              }).then(function(){
+                  reply.code(200).send();
+              })
+              .catch(error => {
+                console.error(`Error saving data to InfluxDB! ${error.stack}`)
+              });
 
     });
 
 }
 module.exports = routes;
-
